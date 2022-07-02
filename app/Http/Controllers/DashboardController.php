08f89d01bc78e0abe70e7e69f6749832be0b3a72
ahyas <ahyasw@gmail.com>
2022-07-02 19:08:40 +0900

    Realisasi sub komponen

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,13 +15,17 @@ class DashboardController extends Controller
 
         $tb_nota = DB::table("tb_nota")
         ->where("tb_nota.id_status",3)
-	    ->select("tb_nota.id_akun", DB::raw("SUM(tb_nota.nominal) as realisasi_akun"))
-	    ->groupBy("tb_nota.id_akun");
+        ->join("tb_akun","tb_nota.id_akun","=","tb_akun.id_akun")
+        ->join("tb_sub_komponen","tb_akun.id_sub_komponen","=","tb_sub_komponen.id")
+	    ->select("tb_akun.id_sub_komponen","tb_sub_komponen.kode","tb_sub_komponen.keterangan", DB::raw("SUM(tb_nota.nominal) as realisasi_akun"))
+	    ->groupBy("tb_akun.id_sub_komponen");
 
         $tb_sp2d = DB::table("tb_test_detail_transaksi")
         ->where("tb_test_detail_transaksi.jumlah",">",0)
-	    ->select("tb_test_detail_transaksi.akun", DB::raw("SUM(tb_test_detail_transaksi.jumlah) as realisasi_akun"))
-	    ->groupBy("tb_test_detail_transaksi.akun")
+        ->join("tb_akun","tb_test_detail_transaksi.akun","=","tb_akun.id_akun")
+        ->join("tb_sub_komponen","tb_akun.id_sub_komponen","=","tb_sub_komponen.id")
+	    ->select("tb_akun.id_sub_komponen", "tb_sub_komponen.kode","tb_sub_komponen.keterangan", DB::raw("SUM(tb_test_detail_transaksi.jumlah) as realisasi_akun"))
+	    ->groupBy("tb_akun.id_sub_komponen")
         ->unionAll($tb_nota)
 	    ->get();
 
